1fbb15284394c847c444d9c583a68f8fc4c6fbe9
tatestaccount <temperanceaubie@gmail.com>
2017-03-24 18:54:33 -0700

    rework init

--- app/src/main/java/com/mikeaubie/finalproject/MainActivity.java
+++ app/src/main/java/com/mikeaubie/finalproject/MainActivity.java
@@ -1,13 +1,27 @@
 package com.mikeaubie.finalproject;
 
+import android.content.Context;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+
+
+
+
   }
+
+
 }
