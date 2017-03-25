f92041a79a1bd4e7e5cc5685d3371b497cd1be3a
tatestaccount <temperanceaubie@gmail.com>
2017-03-25 01:43:54 -0700

    nearly full parsing

--- app/src/main/java/com/mikeaubie/finalproject/Models/FetchQuakeData.java
+++ app/src/main/java/com/mikeaubie/finalproject/Models/FetchQuakeData.java
@@ -9,6 +9,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
 
 
 import org.json.JSONArray;
@@ -19,6 +20,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.*;
 import java.io.*;
+import java.util.Date;
 import java.util.Iterator;
 
 import org.json.*;
@@ -49,12 +51,30 @@ public class FetchQuakeData {
                     //Toast.makeText(context, key, Toast.LENGTH_LONG).show();
                     //itterates over the keys
                     if ( response.get(key) instanceof JSONObject ) {
-
                         JSONObject currentChild = (JSONObject)response.get(key);
                         if(key.equalsIgnoreCase("metadata")) {
-                          Toast.makeText(context, key, Toast.LENGTH_LONG).show();
+                          //Toast.makeText(context, key, Toast.LENGTH_LONG).show();
                         } else {
-                          Toast.makeText(context, (currentChild.get("magnitude")).toString(), Toast.LENGTH_LONG).show();
+//                          //make a newQuake and append it
+
+                          JSONObject coordObject = (JSONObject)currentChild.get("geoJSON");
+                          JSONArray coordArray = (JSONArray)coordObject.get("coordinates");
+                          LatLng newCoord = new LatLng(Double.parseDouble(coordArray.get(0).toString()), Double.parseDouble(coordArray.get(1).toString()));
+                          Date date = new Date(); ///  origin_time
+                          int localTimeUTCOffset = Integer.parseInt(currentChild.get("local_time_utc_offset").toString());
+                          int localTimeDSTOffset = Integer.parseInt(currentChild.get("local_time_dst_offset").toString());
+                          double depthInKM = Double.parseDouble(currentChild.get("depth").toString());
+                          Float magnitude = Float.parseFloat(currentChild.get("magnitude").toString());
+                          JSONObject locationObject = (JSONObject)currentChild.get("location");
+                          String locationDescription = locationObject.get("en").toString();
+                          //Toast.makeText(context, locationDescription, Toast.LENGTH_LONG).show();
+
+                          EarthQuake newQuake = new EarthQuake(newCoord, date, localTimeUTCOffset,
+                                  localTimeDSTOffset, depthInKM, magnitude, locationDescription);
+
+                          EarthQuakes.mEarthQuakeList.add(newQuake);
+
+
                         }
 
                     }
